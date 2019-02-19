254566abf547b2d64f9c3e0824657c285447f974
deng <deng@queserser.co.jp>
2019-02-19 19:16:11 +0900

    no message

---
+++ image_link.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="ja" class="ie6 ielt8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 7 ]>    <html lang="ja" class="ie7 ielt8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 8 ]>    <html lang="ja" class="ie8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="ja">
+<!--<![endif]-->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>selectの操作</title>
+
+<script src="lib/cmn_js/jquery-2.2.4.js"></script>
+<script src="lib/cmn_js/jquery.rwdImageMaps.min.js"></script>
+
+<script>
+$(document).ready(function () {
+	$('img.res-img').rwdImageMaps();
+});
+</script>
+
+
+<style>
+
+figure{
+	line-height:0;
+	margin:0;
+	padding:0;
+	width:100%;
+	}
+figure img{
+	width:auto;
+	max-width:100%;
+	}
+
+</style>
+
+
+</head>
+
+<body class="contents_area" id="header">
+
+
+
+<figure><img src="lib/cmn_img/floor02_00.jpg" alt="" usemap="#Map" class="res-img" />
+  <map name="Map">
+    <area shape="poly" coords="116,48,117,70,88,71,87,129,303,130,304,112,361,112,359,47" href="#">
+    <area shape="poly" coords="542,48,543,86,585,86,586,120,596,118,595,130,775,132,778,6,694,5,692,50" href="#">
+    <area shape="poly" coords="109,137,109,226,119,226,122,248,410,250,416,229,417,175,319,175,315,134" href="#">
+    <area shape="poly" coords="585,156,582,201,544,203,541,239,775,240,777,135,600,138,599,156" href="#">
+  </map>
+</figure>
+
+</body>
+</html>
