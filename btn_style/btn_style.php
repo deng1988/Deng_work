71867a8e50cad459e2684fa0ea3fe94605752949
deng <deng@queserser.co.jp>
2019-03-12 19:43:03 +0900

    no message

---
+++ btn_style/btn_style.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="ja" class="ie6 ielt8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 7 ]>    <html lang="ja" class="ie7 ielt8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 8 ]>    <html lang="ja" class="ie8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="ja">
+<!--<![endif]-->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>BOX-STYLE</title>
+<style>
+
+body{
+	padding:0 !important;
+	margin:0 !important;
+	}
+
+
+p{
+	margin:0;
+	}
+
+</style>
+
+</head>
+
+
+
+<body class="contents_area" id="header">
+
+
+<div>
+<p class="btn-style01">あｓだｓだｄ</p>
+
+
+</div>
+
+
+
+
+
+</body>
+</html>
