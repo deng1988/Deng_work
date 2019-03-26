2c27640454b7de644c5df59c219e8d5a1cb781d6
deng <deng@queserser.co.jp>
2019-03-26 22:16:05 +0900

    no message

---
+++ hover_kaki/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="ja" class="ie6 ielt8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 7 ]>    <html lang="ja" class="ie7 ielt8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if IE 8 ]>    <html lang="ja" class="ie8" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="ja">
+<!--<![endif]-->
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<title>Hover-kaki</title>
+
+<link rel="stylesheet" href="uniq.css">
+
+</head>
+
+
+
+<body class="contents_area">
+
+
+
+
+<ul class="hover-kaki-ul">
+
+<li class="about"><a href="#">CONCEPT</a></li>
+
+<li class="menu"><a href="#">PLAN</a></li>
+
+<li class="price"><a href="#">PRICE</a></li>
+
+<li class="flow"><a href="#">FLOW</a></li>
+
+<li class="staff"><a href="#">STAFF</a></li>
+
+<li class="faq"><a href="#">FAQ</a></li>
+
+
+</ul>
+
+
+<p class="heart-chap">
+	
+</p>
+
+
+
+</body>
+</html>
